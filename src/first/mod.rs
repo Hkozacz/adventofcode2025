05861180dfd289fca05628f932889cb726aaa30c
hkozacz <hubert.kozacz@openx.com>
2025-12-05 09:47:51 +0100

    format

--- src/first/mod.rs
+++ src/first/mod.rs
@@ -3,49 +3,43 @@ enum Direction {
     Right,
     Left,
 }
-struct Instruction{
+struct Instruction {
     direction: Direction,
-    steps: i32
+    steps: i32,
 }
 
-pub fn first_day(){
+pub fn first_day() {
     let input = parse_input();
     let password = follow_instructions(&input);
     println!("Password: {}", password)
 }
 
-fn parse_input() -> Vec<Instruction>{
-    let input = fs::read_to_string("inputs/first_input.txt")
-        .expect("File input for 01 not found");
+fn parse_input() -> Vec<Instruction> {
+    let input = fs::read_to_string("inputs/first_input.txt").expect("File input for 01 not found");
     let mut instructions = Vec::new();
-    for line in input.lines(){
+    for line in input.lines() {
         let direction: Direction;
         if line[0..1].matches("R").count() == 1 {
             direction = Direction::Right;
-        }
-        else if line[0..1].matches("L").count() == 1 {
+        } else if line[0..1].matches("L").count() == 1 {
             direction = Direction::Left;
-        }
-        else {
+        } else {
             panic!("Could not parse line {}", line);
         }
-        instructions.push(
-            Instruction{
-                direction,
-                steps: line[1..].parse::<i32>().unwrap()
-            }
-        );
+        instructions.push(Instruction {
+            direction,
+            steps: line[1..].parse::<i32>().unwrap(),
+        });
     }
     instructions
 }
 
-fn follow_instructions(instructions: &Vec<Instruction>) -> i32{
+fn follow_instructions(instructions: &Vec<Instruction>) -> i32 {
     let mut password = 0;
     let mut state = 50;
-    for ins in instructions{
+    for ins in instructions {
         print!("{} | {} \n", ins.steps % 100, ins.steps);
         match ins.direction {
-
             Direction::Right => {
                 state += ins.steps % 100;
                 if state > 99 {
